0e90dd40666ddb355f30e2cf84974f996ec17692
Shane Miller <shanemiller89@gmail.com>
2019-07-31 20:57:39 -0500

    branch complete

--- src/components/library/LanguageCard.js
+++ src/components/library/LanguageCard.js
@@ -1,45 +1,52 @@
 import React, { Component } from "react";
 import { Card, Image, Grid, Dropdown, Confirm } from "semantic-ui-react";
 
-export default class Languages extends Component {
-state = {
-  open: false
-}
+// TODO:
+// 1.Get delete/Edit button menu to show on right side of title, not below
 
-open = () => this.setState({ open: true });
-close = () => this.setState({ open: false });
+export default class Languages extends Component {
+  state = {
+    open: false
+  };
+
+  open = () => this.setState({ open: true });
+  close = () => this.setState({ open: false });
 
   render() {
     return (
       <React.Fragment>
         <Grid.Column floated="left">
-        <Card>
-          <Image src={this.props.language.imageURL} wrapped ui={false} />
-          <Card.Content>
-            <Card.Header>{this.props.language.title}</Card.Header>
-            <Dropdown icon="list" floated="right">
-              <Dropdown.Menu position="right">
-                <Dropdown.Item
-                  icon="pencil"
-                  description="Edit"
-                  // onClick=
-                />
-                <Dropdown.Item
-                  icon="trash alternate"
-                  description="Delete"
-                  onClick={this.open}
-                />
-                <Confirm
-                  open={this.state.open}
-                  onCancel={this.close}
-                  onConfirm={() =>
-                    this.props.deleteLanguage(this.props.language.id)
-                  }
-                />
-              </Dropdown.Menu>
-            </Dropdown>
-          </Card.Content>
-        </Card>
+          <Card>
+            <Image src={this.props.language.imageURL} wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>{this.props.language.title}</Card.Header>
+              <Dropdown icon="list" floated="right">
+                <Dropdown.Menu position="right">
+                  <Dropdown.Item
+                    icon="pencil"
+                    description="Edit"
+                    // onClick=
+                  />
+                  <Dropdown.Item
+                    icon="trash alternate"
+                    description="Delete"
+                    onClick={this.open}
+                  />
+                  <Confirm
+                    size="mini"
+                    header="Delete Library"
+                    content="Are you sure you want to delete this library?"
+                    confirmButton="Yes"
+                    open={this.state.open}
+                    onCancel={this.close}
+                    onConfirm={() =>
+                      this.props.deleteLanguage(this.props.language.id)
+                    }
+                  />
+                </Dropdown.Menu>
+              </Dropdown>
+            </Card.Content>
+          </Card>
         </Grid.Column>
       </React.Fragment>
     );
