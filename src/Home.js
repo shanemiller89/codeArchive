e41145c36213c52fd1a1f902e53970b00acebbda
Shane Miller <shanemiller89@gmail.com>
2019-08-07 18:08:55 -0500

    image change begun

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Header, Image } from "semantic-ui-react";
+import { Header, Image, Container, List, Segment } from "semantic-ui-react";
 import API from "./modules/API";
 
 export default class Home extends Component {
@@ -17,19 +17,48 @@ export default class Home extends Component {
 
   render() {
     return (
-      <div>
-        <Header as="h1">User Profile</Header>
+      <React.Fragment>
+        <Container
+          style={{
+            background: "#E8E8E8",
+            height: "9em",
+            color: "#15CA00",
+            padding: "1em"
+          }}
+          fluid
+        >
+          <Header style={{ fontSize: "5em", color: "#15CA00" }}>
+            User Profile
+          </Header>
+          <br />
+        </Container>
         <br />
-        <div>
-          {this.state.userInfo.map(userInfo => (
-            <div key={userInfo.id}>
-              <Image src={userInfo.profile} size="medium" circular />
-              <h2 style={{color: "#15CA00"}}>{userInfo.username} </h2>
-              <h3>{userInfo.name} </h3>
+        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly", alignItems: "center"}}>
+          <Segment style={{ width: "30%", height: "60vh", marginTop: "2em", marginLeft: "-5em" }}>
+            <div>
+              {this.state.userInfo.map(userInfo => (
+                <div key={userInfo.id}>
+                  <Image src={userInfo.profile} size="medium" circular />
+                  <h2>
+                    Username:{" "}
+                    <span style={{ color: "#15CA00" }}>
+                      {userInfo.username}{" "}
+                    </span>{" "}
+                  </h2>
+                  <h3>Name: {userInfo.name} </h3>
+                  <h3>Email: {userInfo.email} </h3>
+                </div>
+              ))}
             </div>
-          ))}
+          </Segment>
+          <Segment style={{ width: "40%", height: "60vh" }}>
+            <Header style={{ fontSize: "4em", color: "#15CA00" }}>Stats</Header>
+            <List>
+
+            </List>
+          </Segment>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
