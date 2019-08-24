ae598505fd0230612147389777700624e0149d37
Shane Miller <shanemiller89@gmail.com>
2019-08-24 17:45:57 -0500

    api fix... i hope...

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "codearchive-app.firebaseapp.com",
     databaseURL: "https://codearchive-app.firebaseio.com",
     projectId: "codearchive-app",
     storageBucket: "codearchive-app.appspot.com",
-    messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
   console.log(firebaseConfig)
--- src/modules/API_Key.js
+++ src/modules/API_Key.js
@@ -1,1 +1,1 @@
-export const API_KEY = process.env.GOOGLESEARCH_API_KEY
+export const API_KEY = process.env.REACT_APP_GOOGLESEARCH_API_KEY
