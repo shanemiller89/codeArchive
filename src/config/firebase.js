045413845bf42a5cd4a91c04486f95db24108def
Shane Miller <shanemiller89@gmail.com>
2019-08-24 16:49:04 -0500

    deploy 1

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "codearchive-app.firebaseapp.com",
+    databaseURL: "https://codearchive-app.firebaseio.com",
+    projectId: "codearchive-app",
+    storageBucket: "codearchive-app.appspot.com",
+    messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+  };
---
+++ src/modules/API_Key.js
@@ -0,0 +1,1 @@
+export const API_KEY = process.env.GOOGLESEARCH_API_KEY
