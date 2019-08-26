5758557c13460179bdf00edb888e9980de190f16
Shane Miller <shanemiller89@gmail.com>
2019-08-26 09:15:03 -0500

    fix

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "codearchive-app.firebaseapp.com",
+  databaseURL: "https://codearchive-app.firebaseio.com",
+  projectId: "codearchive-app",
+  storageBucket: "codearchive-app.appspot.com",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
